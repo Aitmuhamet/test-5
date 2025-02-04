7838ea0542b7005b935fbd3e1ffd1fe396ffefbb
Aitmukhamet Khojakeldiyev <xa210296@gmail.com>
2025-02-04 17:20:16 +0500

    fix: typescript imagetools configuration

---
+++ src/declaration.d.ts
@@ -0,0 +1,20 @@
+// src/declaration.d.ts
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.png?w=800&webp' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.jpeg' {
+    const value: string;
+    export default value;
+}
